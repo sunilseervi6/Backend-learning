2359862f7332a1c93c9604302c67278f1431d3c7
sunil <sunilseervik6@gmail.com>
2025-07-23 19:28:58 +0530

    Update .gitignore and clean untracked files

---
+++ script.js
@@ -0,0 +1,8 @@
+// let math = require("./math");
+// console.log(math.sum(2,2));
+
+// let info = require("./Fruits")
+// console.log(info);
+
+import {sum} from "./math.js";
+console.log(sum(5,4));
