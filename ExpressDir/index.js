01f6761e83c72a63b45d46e293233222044f2b24
sunil <sunilseervik6@gmail.com>
2025-07-25 12:17:02 +0530

    learned handling request and response

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -6,14 +6,21 @@ let port =8080
 app.listen(port , ()=>{
     console.log(`listening on port ${port}`);
 })
-app.use((req,res)=>{
-    console.log("received"); 
+// app.use((req,res)=>{
+//     console.log("received"); 
 
-    //res.send("sent")
+//     res.send("sent")
     
-    // res.send({
-    //     fruit:"apple"
-    // });
-    let fruit = "<h1>Fruits</h1> <ul>Apple</ul>"
-    res.send(fruit);
-})
+//     // res.send({
+//     //     fruit:"apple"
+//     // });
+//     // let fruit = "<h1>Fruits</h1> <ul>Apple</ul>"
+//     // res.send(fruit);
+// })
+
+// app.get("/",(resq, res)=>{
+//     res.send("you sent to root path");
+// })
+// app.get("/apple", (req, res)=>{
+//     res.send("you sent to path apple");
+// })
