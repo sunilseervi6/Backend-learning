71de5a1ecc5e8b10ffbc1f8fec77360eef0a6e35
sunil <sunilseervik6@gmail.com>
2025-07-25 13:16:29 +0530

    path paramters and query strings

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -6,6 +6,9 @@ let port =8080
 app.listen(port , ()=>{
     console.log(`listening on port ${port}`);
 })
+
+// app.use to run for all the request
+
 // app.use((req,res)=>{
 //     console.log("received"); 
 
@@ -18,9 +21,28 @@ app.listen(port , ()=>{
 //     // res.send(fruit);
 // })
 
-// app.get("/",(resq, res)=>{
-//     res.send("you sent to root path");
-// })
+
+// routing
+app.get("/",(resq, res)=>{
+    res.send("hello, i am root");
+})
 // app.get("/apple", (req, res)=>{
 //     res.send("you sent to path apple");
 // })
+
+
+//path parameters
+app.get("/:username/:id", (req,res)=>{
+    let username = req.params.username;
+    let id = req.params.id;
+    res.send(`Welcome to the page of @${username}.`);
+
+})
+
+//Query strings
+app.get("/search", (req, res)=>{
+    console.log(req.query);
+    let {q,p} = req.query;
+     
+    res.send(`You searched for the query: ${q} and page ${p}`);
+})
